d9bed0163ca94ab8ecafd960aa80d870901da4a1
Sandeeb Adhikari <sa2734@njit.edu>
2024-12-07 12:29:40 -0500

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import MainCard from "@/components/MainCard";
 import Image from "next/image";
+import Background from "@/public/images/18.png";
 
 export default function Home() {
   return (
     <>
-      <div className="">Hello</div>
+      <div className=" flex flex-col items-center justify-center w-full h-screen">
+        <div className="relative w-full h-full ">
+          <Image src={Background} alt="background" layout="fill" />
+
+          <div className="absolute w-full h-full flex flex-col items-center justify-center inset-0 gap-2">
+            <MainCard />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        "custom-pattern": "url('/images/17.png')",
+      },
     },
   },
   plugins: [],
