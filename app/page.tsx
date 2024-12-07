e2ef86c620702df2667a8afcfd137954e96f1593
Sandeeb Adhikari <sa2734@njit.edu>
2024-12-07 18:19:46 -0500

    adding div for each illustration

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
-import MainCard from "@/components/MainCard";
+import ResumeCard from "@/components/ResumeCard";
 import Image from "next/image";
-import Background from "@/public/images/18.png";
+import Background from "@/public/images/21.png";
+import ProfileCard from "@/components/ProfileCard";
 
 export default function Home() {
   return (
     <>
       <div className=" flex flex-col items-center justify-center w-full h-screen">
         <div className="relative w-full h-full ">
-          <Image src={Background} alt="background" layout="fill" />
+          <Image src={Background} alt="background" objectFit="cover" />
 
-          <div className="absolute w-full h-full flex flex-col items-center justify-center inset-0 gap-2">
-            <MainCard />
+          <div className="absolute w-full h-full top-[500px] left-[1320px] inset-0 ">
+            <ResumeCard />
+          </div>
+          <div className="absolute w-full h-full top-[415px] left-[1115px] inset-0 ">
+            <ProfileCard />
           </div>
         </div>
       </div>
---
+++ components/ProfileCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div className="h-[125px] w-[125px] bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100"></div>
+  );
+};
+
+export default ProfileCard;
