ce706f882fcf9e4543587d6efa10357cba964f37
Sandeeb Adhikari <sa2734@njit.edu>
2024-12-09 09:27:02 -0500

    added the tooltip duration

--- client/components/TooltipComponent.tsx
+++ client/components/TooltipComponent.tsx
@@ -9,13 +9,13 @@ interface TooltipComponentProps {
 
 const TooltipComponent = ({ children, text }: TooltipComponentProps) => {
   return (
-    <Tooltip.Provider>
+    <Tooltip.Provider skipDelayDuration={300}>
       <Tooltip.Root>
         <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
         <Tooltip.Content
           side="bottom"
           align="center"
-          className="h-full w-full bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-300 text-black text-sm px-2 py-1  shadow-md"
+          className="h-full w-full bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-300 text-black text-sm px-2 py-1 shadow-md"
         >
           {text}
           <Tooltip.Arrow className="fill-white" />
